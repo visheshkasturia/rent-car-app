144d6f5760631c8c4f882c33e0ac1cf7bc848c78
Vishesh Kasturia <visheshkasturia@gmail.com>
2021-10-05 10:47:55 -0400

    Add Book Card

--- src/app/containers/Homepage/index.tsx
+++ src/app/containers/Homepage/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components'
 import tw from 'twin.macro'
+import { BookCard } from '../../components/bookCard';
 import { Navbar } from '../../components/navbar';
 import { TopSection } from './topSection';
 
@@ -20,5 +21,6 @@ export function HomePage() {
     return <PageContainer>
         <Navbar/>
         <TopSection/>
+        <BookCard />
     </PageContainer>
 }
