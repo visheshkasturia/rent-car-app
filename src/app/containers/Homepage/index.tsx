d7467b7a15a1bfea08d1533517850de7b35c7b8f
Vishesh Kasturia <visheshkasturia@gmail.com>
2021-10-04 23:38:46 -0400

    Add Top Section and  Button

--- src/app/containers/Homepage/index.tsx
+++ src/app/containers/Homepage/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components'
 import tw from 'twin.macro'
 import { Navbar } from '../../components/navbar';
+import { TopSection } from './topSection';
 
 const PageContainer = styled.div`
     ${tw`
@@ -17,6 +18,7 @@ const PageContainer = styled.div`
 
 export function HomePage() {
     return <PageContainer>
-        <Navbar></Navbar>
+        <Navbar/>
+        <TopSection/>
     </PageContainer>
 }
