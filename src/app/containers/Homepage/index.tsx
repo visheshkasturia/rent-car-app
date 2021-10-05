bc6bbb17ee6656746a6f83451063ce8a4facd34d
Vishesh Kasturia <visheshkasturia@gmail.com>
2021-10-05 18:28:50 -0400

    Add Steps

--- src/app/containers/Homepage/index.tsx
+++ src/app/containers/Homepage/index.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import styled from 'styled-components'
-import tw from 'twin.macro'
-import { BookCard } from '../../components/bookCard';
-import { Marginer } from '../../components/marginer';
-import { Navbar } from '../../components/navbar';
-import { TopSection } from './topSection';
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+import { BookCard } from "../../components/bookCard";
+import { Marginer } from "../../components/marginer";
+import { Navbar } from "../../components/navbar";
+import { BookingSteps } from "./bookingSteps";
+import { TopSection } from "./topSection";
 
 const PageContainer = styled.div`
-    ${tw`
+  ${tw`
         flex
         flex-col
         w-full
@@ -15,14 +16,16 @@ const PageContainer = styled.div`
         items-center
         overflow-x-hidden
         `};
-
 `;
 
 export function HomePage() {
-    return <PageContainer>
-        <Navbar/>
-        <TopSection/>
-        <Marginer direction = "vertical" margin="2em"/> 
-        <BookCard />
+  return (
+    <PageContainer>
+      <Navbar />
+      <TopSection />
+      <Marginer direction="vertical" margin="2em" />
+      <BookCard />
+      <BookingSteps />
     </PageContainer>
+  );
 }
