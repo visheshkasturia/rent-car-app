2641925c6c8ed90c03ba29e628c9d319c922e15a
Vishesh Kasturia <visheshkasturia@gmail.com>
2021-10-06 02:17:06 -0400

    Add Footer

--- src/app/components/logo/index.tsx
+++ src/app/components/logo/index.tsx
@@ -2,50 +2,51 @@ import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
 
-import CarLogoImg from "../../../assets/images/car-logo.png"
+import CarLogoImg from "../../../assets/images/car-logo.png";
+import CarLogoDarkImg from "../../../assets/images/car-logo-dark.png";
 
-const LogoContainer = styled.div `
-    ${tw`
-        flex
-        items-center
+interface ILogoProps {
+  color?: "white" | "dark";
+  bgColor?: "white" | "dark";
+}
 
-    
-    `
-    };
+const LogoContainer = styled.div`
+  ${tw`
+    flex
+    items-center
+  `};
 `;
 
 const LogoText = styled.div`
-    ${tw`
-        text-xl
-        md: text-2xl
-        font-bold
-        text-black
-        m-1
-    `
-    };
-
-
-`;
-
+  ${tw`
+    text-xl
+    md:text-2xl
+    font-bold
+    text-black
+    m-1
+    `};
+  ${({ color }: any) => (color === "white" ? tw`text-white` : tw`text-black`)}
+` as any;
 
 const Image = styled.div`
-    width: auto;
-    ${tw`
-    h-6 
-    md: h-9
-    `};
+  width: auto;
+  ${tw`h-6 md:h-9`};
 
-    img {
-        width : auto;
-        height: auto
-    }
+  img {
+    width: auto;
+    height: 100%;
+  }
 `;
 
-export function Logo(){
-    return <LogoContainer>
-       <Image>
-           <img src={CarLogoImg} />
-        </Image>
-        <LogoText>YourCar.</LogoText>
+export function Logo(props: ILogoProps) {
+  const { color, bgColor } = props;
+
+  return (
+    <LogoContainer>
+      <Image>
+        <img src={bgColor === "dark" ? CarLogoDarkImg : CarLogoImg} />
+      </Image>
+      <LogoText color={color || "dark"}>Yourcar.</LogoText>
     </LogoContainer>
+  );
 }
